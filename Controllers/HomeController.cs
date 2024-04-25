80e04de30be83f800ae6523386d3a1ed2e98bee9
furkansari <furkansari@3eendustriyel.com.tr>
2024-04-25 11:50:42 +0300

    First Commit, does not complated.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using FormsApp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace FormsApp.Controllers;
+
+public class HomeController : Controller
+{
+    public HomeController()
+    {
+        
+    }
+
+    [HttpGet]
+    public IActionResult Index(string searchString, string category)
+    {
+        var products = Repository.Products;
+
+        if(!string.IsNullOrEmpty(searchString))
+        {
+            ViewBag.SearchString=searchString;
+            products = products.Where(t=>t.Name.ToLower().Contains(searchString.ToLower())).ToList();
+        }
+
+        if(!string.IsNullOrEmpty(category) && category != "0")
+        {
+            products = products.Where(t=>t.CategoryId == int.Parse(category)).ToList();
+        }
+        
+        var model = new ProductViewModel{
+            Products=products,
+            Categories=Repository.Categories,
+            SelectedCategory = category
+        };
+
+        //ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name",category);
+
+        return View(model);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name");
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Product model)
+    {
+        if(ModelState.IsValid)
+        {
+            model.ProductId = Repository.Products.Count + 1;
+            Repository.CreateProduct(model);
+            return RedirectToAction("Index");
+        }
+       
+        ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name");
+        return View(model);
+
+    }
+
+}
---
+++ Models/Product.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace FormsApp.Models
+{
+    public class Product
+    {
+        [Display(Name = "Ürün Id")]
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage ="Ürün Adı giriniz.")]
+        [StringLength(100)]
+        [Display(Name = "Ürün Adı")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [Range(0,1000000, ErrorMessage ="Fiyat uygun değil.")]
+        [Display(Name = "Fiyat")]
+        public decimal? Price { get; set; }
+        
+        [Display(Name = "Resim")]
+        public string Image { get; set; } = string.Empty;
+
+        [Display(Name = "Ürün Aktif mi?")]
+        public bool IsActive { get; set; }
+
+        [Required]
+        [Display(Name = "Kategori")]
+        public int? CategoryId { get; set; }
+    }
+}
